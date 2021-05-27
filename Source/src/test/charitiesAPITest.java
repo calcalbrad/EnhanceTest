500b402bdfdd52c150d90e33b729f312a62e5317
Callum <callumbradding@gmail.com>
2021-05-27 14:47:20 +1200

    Removed unused import

--- Source/src/test/charitiesAPITest.java
+++ Source/src/test/charitiesAPITest.java
@@ -10,7 +10,6 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
